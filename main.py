a503772ac6c77085bae8bbbacde44feed38ad7e7
CarlosPKR <jcarlos.pkr@icloud.com>
2026-04-08 15:05:05 +0200

    Refactor: rename folders and update paths

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from src.scraper import scrape_vandal_games
+from source.scraper import scrape_vandal_games
 import pandas as pd
 
 if __name__ == "__main__":
-    data = scrape_vandal_games(limit = 1000)
+    data = scrape_vandal_games(limit = 1)
     df = pd.DataFrame(data)
-    df.to_csv("data/dataset_vandal.csv", index=False, sep=";", encoding="utf-8-sig")
+    df.to_csv("dataset/dataset_vandal.csv", index=False, sep=";", encoding="utf-8-sig")
 
     print("¡Scraping finalizado!")
